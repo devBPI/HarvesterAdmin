fd79a0c613f8bad51ac484e3be9efd6f9ed79f1a
Stagiaire INFO <stagiaire.info@bpi.fr>
2023-06-16 16:33:52 +0200

    Accueil.php

--- Controlleur/HistoriqueMoisson.php
+++ Controlleur/HistoriqueMoisson.php
@@ -4,9 +4,7 @@ if (! $ini) {
     $ini = @parse_ini_file("../etc/default.ini", true);
 }
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+include ("../Composant/ErrorReportingConfig.php");
 
 $rows = 20;
 
