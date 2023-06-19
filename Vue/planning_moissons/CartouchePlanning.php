684b9c21feacdb4a9ca4aa505773b2ab690fa421
Stagiaire INFO <stagiaire.info@bpi.fr>
2023-06-19 10:42:34 +0200

    Correctifs suite à l'affichage des erreurs dans env dev/test

--- Vue/planning_moissons/CartouchePlanning.php
+++ Vue/planning_moissons/CartouchePlanning.php
@@ -3,7 +3,7 @@
 //ini_set("display_errors", 1);
 //error_reporting(E_ALL);
 
-include("../../Composant/ErrorReportingConfig.php");
+include("../Composant/ErrorReportingConfig.php");
 
 Gateway::connection();
 
