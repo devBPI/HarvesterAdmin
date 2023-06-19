9787e2ccb2dfbcf325e9d351472748a4b8051e07
Stagiaire INFO <stagiaire.info@bpi.fr>
2023-06-19 10:25:47 +0200

    Correctifs suite à l'affichage des erreurs dans env dev/test

--- Vue/planning_moissons/CartoucheHistorique.php
+++ Vue/planning_moissons/CartoucheHistorique.php
@@ -1,7 +1,9 @@
 <!-- PHP de la cartouche contenant un tableau affichant l'historique pour une configuration -->
 <?php
-ini_set("display_errors", 0);
-error_reporting(0);
+
+// ini_set("display_errors", 1);
+// error_reporting(E_ALL);
+include("../Composant/ErrorReportingConfig.php");
 
 Gateway::connection();
 
